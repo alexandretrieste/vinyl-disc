dcf68937a49e23868a1a7803c18313b9e4794394
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-07-19 23:05:47 +0000

    feat: new component - login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+.login-wrapper {
+  background-color: #FFFCE;
+}
+
+.form-control {
+  background-color: #FFFFFF;
+  border-radius: 100px;
+}
+
+.forgot-password {
+  color: #B86854;
+}
+
+.login-btn {
+  background-color: #B86854;
+  color: #000000;
+  border-radius: 100px;
+}
+
+.signup {
+  color: #FFFFFF;
+}
+
+.signup-link {
+  color: #B86854;
+}
